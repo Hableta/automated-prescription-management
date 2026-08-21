b1f7c922cf8a33b10707534caf4404b124f9f549
Hableta <hableta@Habletas-MacBook-Air.local>
2026-08-20 21:32:58 -0400

    Clean up project structure

--- docs/database/backend/backend/src/main/java/com/prescription/backend/src/main/java/com/prescription/patient/backend/src/main/java/com/prescription/patient/backend/src/main/java/com/prescription/patient/backend/src/main/java/com/prescription/prescription/backend/src/main/java/com/prescription/prescription/PrescriptionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.prescription.prescription;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
-}
